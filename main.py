bff8f50f384bfdf3db0380790702e8973b160a0a
Harshitha S D <keshuharshu@gmail.com>
2026-02-19 21:30:15 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+number=int(input("enter a number"))
+square=number*number
+print("the square of the number is",square)
+if square%2==0:
+    print("the number is even")
+else:
+    print("the number is odd")
